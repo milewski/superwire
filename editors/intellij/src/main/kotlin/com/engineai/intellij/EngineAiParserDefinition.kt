3b4b5c898980169251e710a5818819826fc8b067
Rafael Milewski <rafael.milewski@gmail.com>
2026-04-04 17:14:45 +0800

    fix(intellij): enable folding and localize ctrl-click targets

--- editors/intellij/src/main/kotlin/com/engineai/intellij/EngineAiParserDefinition.kt
+++ editors/intellij/src/main/kotlin/com/engineai/intellij/EngineAiParserDefinition.kt
@@ -1,6 +1,64 @@
 package com.engineai.intellij
 
+import com.intellij.lang.ASTNode
 import com.intellij.lang.ParserDefinition
-import com.intellij.openapi.fileTypes.PlainTextParserDefinition
+import com.intellij.lang.PsiParser
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.project.Project
+import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IFileElementType
+import com.intellij.psi.tree.TokenSet
+import com.intellij.extapi.psi.ASTWrapperPsiElement
 
-class EngineAiParserDefinition : ParserDefinition by PlainTextParserDefinition()
+class EngineAiParserDefinition : ParserDefinition {
+    override fun createLexer(project: Project?): Lexer {
+        return EngineAiLexer()
+    }
+
+    override fun createParser(project: Project?): PsiParser {
+        return PsiParser { rootElementType, psiBuilder ->
+            val rootMarker = psiBuilder.mark()
+
+            while (!psiBuilder.eof()) {
+                psiBuilder.advanceLexer()
+            }
+
+            rootMarker.done(rootElementType)
+            psiBuilder.treeBuilt
+        }
+    }
+
+    override fun getFileNodeType(): IFileElementType {
+        return EngineAiElementTypes.FILE
+    }
+
+    override fun getCommentTokens(): TokenSet {
+        return TokenSet.EMPTY
+    }
+
+    override fun getStringLiteralElements(): TokenSet {
+        return TokenSet.EMPTY
+    }
+
+    override fun createElement(node: ASTNode): PsiElement {
+        return ASTWrapperPsiElement(node)
+    }
+
+    override fun createFile(viewProvider: FileViewProvider): PsiFile {
+        return EngineAiPsiFile(viewProvider)
+    }
+
+    override fun spaceExistenceTypeBetweenTokens(left: ASTNode, right: ASTNode): ParserDefinition.SpaceRequirements {
+        return ParserDefinition.SpaceRequirements.MAY
+    }
+}
+
+class EngineAiPsiFile(viewProvider: FileViewProvider) : com.intellij.extapi.psi.PsiFileBase(viewProvider, EngineAiLanguage) {
+    override fun getFileType() = EngineAiFileType
+
+    override fun toString(): String {
+        return "Engine AI File"
+    }
+}
