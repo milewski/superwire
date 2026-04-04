1ec685e66043a988d006732ebb3bdb5931d0c4d6
Rafael Milewski <rafael.milewski@gmail.com>
2026-04-04 15:02:18 +0800

    revert changes

--- crates/ffi/javascript/src/Bridge/EngineFfiBridge.ts
+++ crates/ffi/javascript/src/Bridge/EngineFfiBridge.ts
@@ -1,8 +1,8 @@
 import { close, DataType, load, open, restorePointer, wrapPointer } from 'ffi-rs'
 
-import { FFI_LIBRARY_KEY, FFI_OPERATION, FFI_PROTOCOL_VERSION } from './constants'
-import { resolveDefaultLibraryPath } from './library-path'
-import type { EngineFfiBridgeOptions, FfiBoundaryEnvelope, FfiInvokeRequestEnvelope, FfiResponseEnvelope, ReadExecutionValueEnvelope, ReadExecutionValueRequest, RequestOptions } from './types'
+import { FFI_LIBRARY_KEY, FFI_OPERATION, FFI_PROTOCOL_VERSION } from '../Protocol/constants'
+import { resolveDefaultLibraryPath } from '../library-path'
+import type { EngineFfiBridgeOptions, FfiBoundaryEnvelope, FfiInvokeRequestEnvelope, FfiResponseEnvelope, ReadExecutionValueEnvelope, ReadExecutionValueRequest, RequestOptions } from '../types'
 
 export class EngineFfiBridge {
     private readonly libraryPath: string
