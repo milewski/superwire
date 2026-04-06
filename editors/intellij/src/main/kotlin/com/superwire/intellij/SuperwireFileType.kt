d5b42f41445bec18442cfebab0e94faf5575e538
Rafael Milewski <rafael.milewski@gmail.com>
2026-04-06 22:46:37 +0800

    add intellij plugin icon for superwire files

--- editors/intellij/src/main/kotlin/com/superwire/intellij/SuperwireFileType.kt
+++ editors/intellij/src/main/kotlin/com/superwire/intellij/SuperwireFileType.kt
@@ -1,9 +1,12 @@
 package com.superwire.intellij
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object SuperwireFileType : LanguageFileType(SuperwireLanguage) {
+    private val fileIcon = IconLoader.getIcon("/icons/icon.svg", SuperwireFileType::class.java)
+
     override fun getName(): String {
         return SuperwirePluginConstants.LANGUAGE_NAME
     }
@@ -17,6 +20,6 @@ object SuperwireFileType : LanguageFileType(SuperwireLanguage) {
     }
 
     override fun getIcon(): Icon? {
-        return null
+        return fileIcon
     }
 }
