79ed8c65419d358e4f16760f33e32a3f5643d6d9
Rafael Milewski <rafael.milewski@gmail.com>
2026-04-12 10:24:08 +0800

    fix WeatherAgentInput and WeatherBoundInput to extend Spatie Data

--- integration/superwire-laravel/src/Tools/Data/WeatherAgentInput.php
+++ integration/superwire-laravel/src/Tools/Data/WeatherAgentInput.php
@@ -4,9 +4,10 @@ declare(strict_types = 1);
 
 namespace Superwire\Laravel\Tools\Data;
 
+use Spatie\LaravelData\Data;
 use Superwire\Laravel\Contracts\ToolInputData;
 
-final readonly class WeatherAgentInput implements ToolInputData
+final class WeatherAgentInput extends Data implements ToolInputData
 {
     public function __construct(public ?string $city = null)
     {
--- integration/superwire-laravel/src/Tools/Data/WeatherBoundInput.php
+++ integration/superwire-laravel/src/Tools/Data/WeatherBoundInput.php
@@ -4,9 +4,10 @@ declare(strict_types = 1);
 
 namespace Superwire\Laravel\Tools\Data;
 
+use Spatie\LaravelData\Data;
 use Superwire\Laravel\Contracts\ToolBoundInputData;
 
-final readonly class WeatherBoundInput implements ToolBoundInputData
+final class WeatherBoundInput extends Data implements ToolBoundInputData
 {
     public function __construct(public ?string $city = null)
     {
