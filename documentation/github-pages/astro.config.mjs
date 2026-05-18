24e8ff389bd41d45c82381a448824e0e4569c0df
Rafael Milewski <rafael.milewski@gmail.com>
2026-05-18 08:55:59 +0800

    update astro base path

--- documentation/github-pages/astro.config.mjs
+++ documentation/github-pages/astro.config.mjs
@@ -2,11 +2,8 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/').at(1);
-const basePath = process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}` : '/';
-
 export default defineConfig({
-  base: basePath,
+  base: '/',
   integrations: [react()],
   output: 'static',
   vite: {
